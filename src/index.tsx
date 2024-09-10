dabeee41e28df380be25f05b68028b79bbe1daf8
isoppp <prog@d2deck.com>
2024-09-10 23:02:33 +0900

    add nonce example

--- src/index.tsx
+++ src/index.tsx
@@ -4,14 +4,19 @@ import { cors } from 'hono/cors'
 import type { FC } from 'hono/jsx'
 import { secureHeaders } from 'hono/secure-headers'
 
-const app = new Hono()
+type Variables = {
+  nonce: string
+}
+const app = new Hono<{ Variables: Variables }>()
+import crypto from 'node:crypto'
 import { serveStatic } from '@hono/node-server/serve-static'
 import { logger } from 'hono/logger'
 
 // ngrok path
-const BASE_URL = ''
+const BASE_URL = ''.replace(/\/$/, '')
 const withBasePath = (path: string) => BASE_URL + path
 
+app.use('*', cors())
 app.use(logger())
 app.use(
   '/public/*',
@@ -22,36 +27,21 @@ app.use(
     },
   }),
 )
-app.use('*', cors())
-app.use(
-  '*',
-  secureHeaders({
-    reportingEndpoints: [
-      {
-        name: 'csp-endpoint',
-        url: withBasePath('/report'),
-      },
-    ],
-    contentSecurityPolicy: {
-      defaultSrc: [''],
-      reportTo: 'csp-endpoint',
-      connectSrc: ["'self'"],
-      imgSrc: ["'self'", 'data:'],
-      scriptSrc: ["'self'"],
-      styleSrc: ["'self'", 'https:', "'unsafe-inline'"],
-    },
-  }),
-)
 
-const Sample: FC = () => {
+const Sample: FC<{ pathname: string; nonce?: string }> = ({ nonce }) => {
   return (
     <html>
       <head>
         <title>CSP Header Playground</title>
         <link rel='stylesheet' href={withBasePath('/public/sample.css')} />
-        <script src={withBasePath('/public/sample.js')} />
+        <script src={withBasePath('/public/sample.js')} nonce={nonce} />
       </head>
       <body>
+        <div style={{ display: 'flex', gap: '20px', padding: '10px', borderBottom: '2px solid gray' }}>
+          <a href='/'>/</a>
+          <a href='/csp'>/csp</a>
+          <a href='/nonce'>/nonce</a>
+        </div>
         <div class='hello'>hello</div>
       </body>
     </html>
@@ -63,10 +53,52 @@ app.get('/', async (c) => {
   return c.html(<Sample />)
 })
 
+app.use(
+  '/csp',
+  secureHeaders({
+    reportingEndpoints: [
+      {
+        name: 'csp-endpoint',
+        url: withBasePath('/report'),
+      },
+    ],
+    contentSecurityPolicy: {
+      defaultSrc: [''],
+      reportTo: 'csp-endpoint',
+      // connectSrc: ["'self'"],
+      // imgSrc: ["'self'", 'data:'],
+      // scriptSrc: ["'self'"],
+      // styleSrc: ["'self'", 'https:', "'unsafe-inline'"],
+    },
+  }),
+)
+app.get('/csp', async (c) => {
+  await new Promise((resolve) => setTimeout(resolve, 250))
+  return c.html(<Sample />)
+})
+
 app.post('/report', async (c) => {
   console.log('got report')
   console.log(await c.req.json())
   return c.text('ok')
+})
+
+// nonce
+app.use('*', (c, next) => {
+  c.set('nonce', crypto.randomBytes(16).toString('hex'))
+  return next()
+})
+app.use(
+  '/nonce',
+  secureHeaders({
+    contentSecurityPolicy: {
+      scriptSrc: [(c) => `'nonce-${c.get('nonce')}'`],
+    },
+  }),
+)
+app.get('/nonce', async (c) => {
+  await new Promise((resolve) => setTimeout(resolve, 1000))
+  return c.html(<Sample nonce={c.get('nonce')} />)
 })
 
 const port = Number(process.env.PORT) || 3000
